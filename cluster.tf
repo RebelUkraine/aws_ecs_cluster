f1dd25107f63776e00046cd44adac68782b34ffd
RebelUkraine <support4uf1@gmail.com>
2025-05-28 21:49:37 +0300

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -1,3 +1,8 @@
+data "aws_ssm_parameter" "ecs_ami" {
+  name   = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  region = "eu-north-1"
+}
+
 # 1. ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = "main"
@@ -7,7 +12,7 @@ resource "aws_ecs_cluster" "main" {
 resource "aws_launch_template" "ecs" {
   name          = "ecs-launch-template-rebel-grease"
   # name_prefix   = "ecs-launch-template-"
-  image_id      = "ami-001c4ccee38ce5932" # Replace with your ECS-optimized AMI
+  image_id      = data.aws_ssm_parameter.ecs_ami.value # Replace with your ECS-optimized AMI
   instance_type = "t3.micro"
 
   user_data = base64encode(<<EOF
