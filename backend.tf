08988cafa5d58f560dfe118856fedafef409d464
RebelUkraine <support4uf1@gmail.com>
2025-05-28 20:35:00 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ backend "s3" {
     bucket = "rebelgrease-state"
     key    = "terraform.tfstate"
     region = "eu-north-1"
-    dynamodb_table = "tf_lockid"
+    dynamodb_table = "terraform-statefile"
 }
 }
