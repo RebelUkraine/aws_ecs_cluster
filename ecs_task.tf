757b7aa422f8086f8a9a076320745ef8a8f55835
RebelUkraine <support4uf1@gmail.com>
2025-06-16 23:52:36 +0300

    Update ecs_task.tf

--- ecs_task.tf
+++ ecs_task.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_task_definition" "node-red" {
   container_definitions = jsonencode([
     {
       name      = "first"
-      image     = "service-first"
+      image     = "633087887197.dkr.ecr.eu-north-1.amazonaws.com/node-red:latest"
       cpu       = 10
       memory    = 512
       essential = true
