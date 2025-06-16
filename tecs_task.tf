9ea4ab8f9932e831d244b899f2b9ae0ab1bd7b5f
RebelUkraine <support4uf1@gmail.com>
2025-06-16 14:09:54 +0300

    Create tecs_task.tf

---
+++ tecs_task.tf
@@ -0,0 +1,16 @@
+resource "aws_ecs_task_definition" "node-red" {
+  family = "node-red"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 1880
+          hostPort      = 1880
+        }
+      ]
+    }
